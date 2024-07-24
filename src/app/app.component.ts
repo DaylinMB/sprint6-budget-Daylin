684e557ab5a42ad8695529a6d1240d6909d93e48
Daylin Mejia <daylinmejia24@gmail.com>
2024-07-24 10:37:26 +0200

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HomeComponent],
+  imports: [CommonModule, HomeComponent, MatSlideToggleModule],
   template: `<app-home></app-home>`,
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'sprint6-budget-Daylin';
 }
